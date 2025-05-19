0e3a97510b70440888df1324cfc5f7f5e1240424
Luís Fernando Torres <luuisotorres@icloud.com>
2025-05-19 08:06:21 -0300

    Update settings.py

--- src/fastapi_iris_predictor/config/settings.py
+++ src/fastapi_iris_predictor/config/settings.py
@@ -1,19 +1,13 @@
 import os
 from dotenv import load_dotenv
-
 # Load environment variables from .env
 load_dotenv()
-
 # Configure JWT with environment variables
 JWT_SECRET = os.getenv("JWT_SECRET")
 JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
 JWT_EXP_DELTA_SECONDS = int(os.getenv("JWT_EXP_DELTA_SECONDS", "3600"))
-
-
 # Database
 DB_URL = os.getenv("DATABASE_URL")
-
-
 # Username and password
 TEST_USERNAME = os.getenv("TEST_USERNAME")
 TEST_PASSWORD = os.getenv("TEST_PASSWORD")
